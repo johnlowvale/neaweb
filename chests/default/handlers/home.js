9ed4b8232fd10121867f3d6c274768973abfa694
John Lowvale <johnlowvale@hotmail.com>
2016-04-06 14:27:29 +0700

    adjusted some web contents

--- chests/default/handlers/home.js
+++ chests/default/handlers/home.js
@@ -36,8 +36,9 @@ class home_handler {
     cb.connect("localhost","neaweb");
     
     //test sql library query
-    cb.select("*").type("user").query(function(Error,Results){
-      console.log("query:");
+    cb.select("*").type("user").query(function(Error,Results){     
+      console.log("POST /home");
+      console.log("All users:");
       if (Error)
         console.log(Error);
       else {
