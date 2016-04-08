6976caf3dcb6d6db957f2c43743e98bd6ec778b5
John Lowvale <johnlowvale@hotmail.com>
2016-04-08 10:40:28 +0700

    updated to Quickt v1.10

--- chests/default/mains/home.js
+++ chests/default/mains/home.js
@@ -1,4 +1,8 @@
 
+var RED   = "red";
+var GREEN = "green";
+var BLUE  = "blue";
+
 function load_user_list() {
   post("/home",{},function(Error,Data){
     e("User-List").innerHTML = JSON.stringify(Data);
