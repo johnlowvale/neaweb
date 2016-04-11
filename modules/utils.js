1bbf2529facff3854ffd739dd802170a642f1cfd
John Lowvale <johnlowvale@hotmail.com>
2016-04-11 16:05:12 +0700

    added sha256 method in utils.js

--- modules/utils.js
+++ modules/utils.js
@@ -1,18 +1,42 @@
-
+/**
+ * Neaweb - Neat Web Framework in Full Stack JavaScript
+ * Freeware by Stinte Ltd
+ * @file    Utilities file 
+ * @version 0.1
+ * @author  John Lowvale
+ */
 "use strict";
 
 //nodejs modules
 var crypto = require("crypto"); 
 
+/**
+ * Utilities class
+ */
 class utils {
 
+  /**
+   * Constructor
+   */
   constructor() {
   }
-  
+                
+  /**
+   * SHA1 hash
+   */
   static sha1(Text) {
     var Sha1 = crypto.createHash("sha1");
     Sha1.update(Text);
     return Sha1.digest("hex");    
+  }
+              
+  /**
+   * SHA256 hash
+   */
+  static sha256(Text) {
+    var Sha256 = crypto.createHash("sha256");
+    Sha256.update(Text);
+    return Sha256.digest("hex");    
   }
 }
 
