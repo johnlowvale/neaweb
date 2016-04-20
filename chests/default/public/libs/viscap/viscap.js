4abd8def780940abbf68e0926d5f3fd5c625ec17
John Lowvale <johnlowvale@hotmail.com>
2016-04-20 14:41:19 +0700

    updated comments in viscap.js

--- chests/default/public/libs/viscap/viscap.js
+++ chests/default/public/libs/viscap/viscap.js
@@ -1,7 +1,8 @@
 /**
  * Visual captcha component for working with security.js on server
  * Captcha text is a string of iconic unicode characters
- * There's no input box, only dragging icons into an image
+ * There's no input box, only 1 image of 6 icons, clicking on icon on this
+ * image will popup a box of 6x6 icons to choose a match
  */
 
 //end of file
