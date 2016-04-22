15d3e335e6e5568607490610418c7bf7357fc67c
John Lowvale <johnlowvale@hotmail.com>
2016-04-22 10:34:25 +0700

    abandon unicode symbol captcha, use random image list captcha

--- modules/security.js
+++ modules/security.js
@@ -29,11 +29,12 @@ class security {
   }  
       
   /**
-   * Create captcha image from text with skewing, distortion, etc.
+   * Create captcha image from list of images
+   * Resulting captcha image is distorted
    * @return base64 image data
    */                         
-  static text_to_image(Text) {
-    var Width  = Text.length*security.CAPTCHA_HEIGHT;
+  static images_to_captcha(Images) {
+    var Width  = Images.length*security.CAPTCHA_HEIGHT;
     var Height = security.CAPTCHA_HEIGHT;  
     var Image  = pureimage.make(Width,Height);
     
@@ -42,61 +43,22 @@ class security {
   
   /**
    * Get a visual captcha            
-   * Text is a string of unicode icon characters
-   * Image is PNG in base64  
+   * Captlist (captcha image list) is a list of images to make captcha image
+   * Picklist is a list of images as choices for user to choose
+   * Image is the resulting image made from Captlist
    * @param Request containing a session cookie
-   * @return {Text:"...",Picklist:"...",Image:"..."}
+   * @return {Captlist:"...",Picklist:"...",Image:"..."}
    */                      
   static get_captcha(Request) {
-    var Cwd     = process.cwd();
-    var Content = fs.readFileSync(Cwd+"/data/iconics.txt","utf8");
-    
-    //get iconic characters
-    var Iconics = [];
-    for (var Index=0; Index<Content.length; Index++) 
-      if (Content.charCodeAt(Index)>255) {
-        var Character = Content.charAt(Index);
-        Iconics.push(Character);
-      }   
-      
-    //randomly pick CAPTCHA_LENGTH iconics to make Text
-    var Text = "";
-    for (var Index=0; Index<security.CAPTCHA_LENGTH; Index++) {
-      var Jndex     = Math.floor(Iconics.length*Math.random());
-      var Character = Iconics[Jndex];
-      Text += Character;
-    }                   
-                        
-    //randomly add iconics to picklist
-    var Picklist = Text;              
-    for (var Index=security.CAPTCHA_LENGTH; Index<security.CAPTCHA_PICKLIST;
-    Index++) {
-      var Jndex     = Math.floor(Iconics.length*Math.random());
-      var Character = Iconics[Jndex];
-      Picklist += Character;  
-    }     
-    
-    //create image for Text
-    var Image = security.text_to_image(Text);
-        
-    //set captcha value in session
-    session.set(Request,"Captcha",Text);
-    
-    //result
-    return {
-      Text:    Text,
-      Picklist:Picklist,
-      Image:   Image
-    };
+    //
   }   
   
   /**
    * Verify a visual captcha   
    * @return true when captcha is matching
    */                       
-  static verifty_captcha(Request,Text) {
-    var Captcha = session.get(Request,"Captcha");
-    return Text==Captcha;
+  static verifty_captcha(Request,Captlist) {
+    //
   }
 }
 
